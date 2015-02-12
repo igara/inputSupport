2ac20e67bb2e1ff03894fd7d604a778a782462c6
五十嵐 翔 <igara1119@gmail.com>
2015-02-12 18:00:49 +0900

    no message

---
+++ WorkTimeUpDown.js
@@ -0,0 +1,63 @@
+/**
+ * キーバインドで工数を入力
+ * 対応ボタン:↑ ↓
+ */
+
+document.onkeydown = function(event) {
+    var keyEvent = event || window.event;
+	if (keyEvent.keyCode == 38) {
+		workTimeIncrement(getWorkTimeId());
+	} else if (keyEvent.keyCode == 40) {
+		workTimeDecrement(getWorkTimeId());
+	}
+}
+
+/**
+ * 工数丸め処理
+ * @param workTime 
+ * @return 工数を0.25で除算の商に0.25を乗算する
+ */
+function mathRoundWorkTime(workTime) {
+	return Math.round(workTime / 0.25) * 0.25;
+}
+
+/**
+ * 工数部分の要素を取得する
+ * valueが設定されていない時の考慮あり
+ */
+function getWorkTimeId() {
+	var getElementId = document.getElementById(document.activeElement.id);
+	if (String(getElementId.id).match(/^new_time_entry_\d*_\d*_hours/)) {
+		if (getElementId.value != "") {
+			return getElementId;
+		} else {
+			getElementId.setAttribute("value", "0");
+		}
+	}
+}
+
+/**
+ * 工数を0.25ずつ加算
+ */
+function workTimeIncrement(workTime) {
+	if (workTime.value >= 24) {
+		alert("1日何時間？");
+	} else if (workTime.value <= 24 && workTime.value % 0.25 == 0) {
+		workTime.value = parseFloat(workTime.value) + parseFloat(0.25);
+	} else {
+		workTime.value = mathRoundWorkTime(workTime.value);
+	}
+}
+
+/**
+ * 工数を0.25ずつ減算
+ */
+function workTimeDecrement(workTime) {
+	if (workTime.value <= 0) {
+		alert("0より小さい");
+	} else if (workTime.value >= 0 && workTime.value % 0.25 == 0) {
+	    workTime.value = parseFloat(workTime.value) - parseFloat(0.25);
+	} else {
+		workTime.value = mathRoundWorkTime(workTime.value);
+	}
+}
